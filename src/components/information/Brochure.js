014065c7070512597ba54335777f92f3322ea52c
jerrytc7 <jerrykondner@gmail.com>
2022-03-29 12:40:22 -0400

    added brochure images

--- src/components/information/Brochure.js
+++ src/components/information/Brochure.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function Brochure() {
   return (
-    <div>Brochure</div>
-  )
+    <div>
+      <header>
+        <h3>Brochure</h3>
+      </header>
+      <body>
+        <p>
+          Here is the brochure for our school!
+        </p>
+        <img src="mina-brochure-front.png" alt="mina's-school-brochure-front"/>
+        <img src="mina-brochure-back.png" alt="mina's-school-brochure-back"/>
+      </body>
+    </div>
+  );
 }
 
-export default Brochure
+export default Brochure;
