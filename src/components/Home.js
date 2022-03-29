bd271392eeba0b5d04b0a36b9848d6ab3170724c
jerrytc7 <jerrykondner@gmail.com>
2022-03-28 20:04:41 -0400

    added background hero gif with title

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div
+      className="hero-image"
+        style={{
+          backgroundImage: `url("hero-image.gif")`,
+          height: "40rem",
+          width: "",
+          backgroundRepeat: 'no-repeat'
+        }}
+      >
+        <h3 className="mina-title">Mina's School of Great Falls</h3>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
