5dba498a018ad874225695bddf7d8518238afed5
jerrytc7 <jerrykondner@gmail.com>
2022-03-28 20:48:10 -0400

    added math info

--- src/components/subjects/Math.js
+++ src/components/subjects/Math.js
@@ -1,9 +1,205 @@
-import React from 'react'
+import React from "react";
 
 function Math() {
   return (
-    <div>Math</div>
-  )
+    <div>
+      <header>
+        <h3>Mathematics</h3>
+      </header>
+      <body>
+        <p>
+          The method of teaching math we use is one I developed over decades of
+          working successfully with students possessing a wide range of skills.
+          Every student attending our school will advance to a high level of
+          understanding using this methodology. If your child is weak in math it
+          is not because they cannot learn the material, I believe it is due to
+          poor teaching methods. Our kindergartners by third grade typically
+          have completed all elementary mathematics in depth. Basic skills are
+          taught in depth with 90 minutes of math class over the course of each
+          school day. Students develop a strong foundation in math before
+          learning more advanced skills. Only after a student masters each step,
+          does he/she move to the next level. The pace and complexity of the
+          math program is adjusted to the individual child. No student is forced
+          into a one-size-fits-all environment as is often found in larger
+          classroom settings. ​ My concept is simple and effective: all critical
+          knowledge must be learned before advancing from addition and
+          subtraction to multiplication and division. Too often children are
+          passed to the next grade without the requisite skills to handle more
+          advanced math topics. At our school every student, at every level, is
+          provided the intellectual tools needed to do well in mathematics. No
+          one is left behind and there is no upper limit on learning. It is no
+          surprise that a lot of our parents have told me we perform miracles as
+          their children not only do exceedingly well in math but also have
+          grown to love the subject! Regular use of hands on learning tools at
+          all levels, incorporating place-value blocks, physical counting
+          models, and other manipulatives into the program helps students to
+          develop a concrete understanding of abstract numbers and concepts that
+          will last a lifetime. As the children move through the lesson
+          material, previous topics are reviewed to reinforce concepts and to
+          prepare them for more advanced mathematics. In each “packet,” students
+          have numerous opportunities to apply what they have studied using
+          real-world problems. The pace is determined by the ability of the
+          individual child to absorb and use their skills, not by chronological
+          age or an assigned grade level. The result is a math friendly
+          environment in which children as young as first grade enjoy adding or
+          subtracting 4-digit numbers in their head with ease! Your child will
+          excel in math because all of our teachers know how to teach students
+          to think critically and with the confidence and desire to continue
+          tackling higher level math successfully in high school, college and
+          beyond. Ms. Mina Kondner Headmaster
+        </p>
+        <h4>Addition</h4>
+        <p>
+          In the addition lessons, the students use manipulatives such as ten
+          blocks and place value mats. We make sure every student knows each
+          lesson before the child moves on to the next step of addition. We
+          begin by teaching numbers (reading and making numbers). By the end of
+          the addition package, students will have learned to add three-digit
+          numbers to three-digit numbers mentally and to add larger numbers on
+          paper.
+        </p>
+        <h4>Subtraction</h4>
+        <p>
+          In subtraction, we use same manipulatives as in the addition package
+          along with real-world money. Students learn simple subtraction and end
+          up mentally able to subtract numbers in the thousands. They also learn
+          about monetary units and to use their mental math knowledge to handle
+          currency problems. Students also learn how to subtract on paper and to
+          regroup numbers. Finally, they are given a variety of word problems
+          that include addition and subtraction.
+        </p>
+        <h4>Simple Multiplication/Division</h4>
+        <p>
+          For multiplication, students use manipulatives which are called the
+          “spider and caterpillar.” The spider and caterpillar legs are filled
+          with identical groups of beads and then used as a visual aid or hands
+          on multiplication table. Coins and cubes, or counters, provide another
+          way for our students to visually understand multiplication and
+          division.
+        </p>
+        <h4>Advanced Multiplication</h4>
+        <p>
+          The advanced multiplication package teaches students rounding and how
+          to multiply two-digit numbers times a one-digit number mentally.
+          Students complete this topic with the know how to multiply three-digit
+          by two-digit numbers on paper.
+        </p>
+        <h4>Long Division</h4>
+        <p>
+          The long division package starts with the reinforcement of simple
+          division to connect it with the estimation of division. With the
+          estimation skill, many students can easily do long division without
+          any guessing. The long division lesson starts with small long division
+          problems and moves to larger numbers. By the end of the package,
+          students learn about divisibility and practice related word problems.
+          These word problems are made easy through a method called “group, in
+          the group, total.” Using this process, students can perform
+          multiplication, division, times as much, and any other word problems
+          that typically are harder due to the wording and numbers.
+        </p>
+        <h4>Fractions</h4>
+        <p>
+          During the fraction lessons, students use fraction bars to envision
+          and understand each lesson within the packages. Fractions are divided
+          into five packages: introduction, addition, subtraction,
+          multiplication, and division. Each package prepares the child for the
+          next lesson.
+        </p>
+        <h6>Introduction to Fractions</h6>
+        <p>
+          In this package, students learn all there is to know about simple and
+          more complex fractions. Topics included in the beginning level
+          incorporates work that prepares students for the next level, and
+          includes greatest common factor (GCF), Least Common Multiple (LCM),
+          equivalent fractions, comparing fractions and, improper and mixed
+          numbers.
+        </p>
+        <h6>Addition/Subtraction/Multiplication/Division of Fractions</h6>
+        <p>
+          Students use the skills gained in the beginning fraction lessons to
+          add, subtract, multiply, and divide simple fractions and mixed
+          numbers. By the end of this topic students are able to work
+          independently to solve word problems and write down the mathematical
+          process.
+        </p>
+        <h4>Decimals</h4>
+        <p>
+          The decimal package uses a manipulative called the “decimal chart.”
+          Students start by gaining a working knowledge of the meaning of
+          decimals. Then they learn how to do mental math with decimals,
+          including adding, subtracting, and multiplying the numbers. Children
+          add, subtract, and apply these skills to multiplication and division
+          of decimals using a simple method. Students also learn to convert
+          fractions to decimals and vice versa. By the end of this topic,
+          students work on decimal word problems.
+        </p>
+        <h4>Percent</h4>
+        <p>
+          In the percent lessons, students also will use the decimal chart to
+          understand the connection between decimals and percent. Fraction bars
+          create a visual connection between fractions and percent. This helps
+          students convert decimals and fractions to percent and vice versa.
+          After students properly understand each concept in the percent
+          package, they move on to complete 7th grade level percent lessons.
+          Seventh grade percent work includes ratio, rate, unit rate, and
+          proportion. With that knowledge, students can apply proportions to
+          solve percent word problems. They work on word problems containing
+          percent questions and a mix of other topics from earlier lessons. The
+          percent package is considered the end of an elementary level knowledge
+          of mathematics. At this point the level of many of the questions is
+          similar to that found on the SAT test!
+        </p>
+        <h4>Inegers</h4>
+        <p>
+          In our integers package, we use a number line, and blue and red tiles
+          that represent debt and earning (positive and negative). Learning
+          through these manipulatives, students grow to understand completely
+          when they should add or subtract, and when an answer is negative or
+          positive. Students also use their addition and subtraction mental math
+          skills to work with integers. With the knowledge gained from the
+          integers and fraction’s packages, students can easily perform all four
+          operations with negative and positive fractions.
+        </p>
+        <h4>Units of Measurement</h4>
+        <p>
+          In the measurement unit, there is an emphasis on the metric system.
+          Students also learn to use the “customary system” for every day
+          measurement. Students practice the lesson using manipulatives that
+          visually demonstrate the different types of units and their meaning.
+          At the end of this package, students are taught about elapsed time. As
+          in other lessons, the measurement topic requires a lot of practice.
+        </p>
+        <h4>Geometry</h4>
+        <p>
+          The geometry package starts with a simple understanding of shapes.
+          This package uses a combination of practice problems found in the 6th
+          and 7th grade math book. The lessons are taught from those books and
+          manipulatives are used to reinforce each lesson on shapes, lines,
+          angles, etc. Using “nets,” students take a three-dimensional shape and
+          unfold it to create a two-dimensional space figure which they can work
+          with to find the surface area of a solid. The children develop a deep
+          understanding of the relationship between two- and three-dimensional
+          objects. By the end of this package, the student will learn how to
+          find the perimeter, area, and volume of every shape through a concrete
+          understanding of geometry rather than solely memorizing formulas soon
+          forgotten.
+        </p>
+        <h4>Equations</h4>
+        <p>
+          In this package, students learn to combine like terms and by the end
+          to solve for the variable in both simple and complex equations using
+          integers and fractions. They also learn to factor numbers in
+          preparation for algebra.
+        </p>
+        <h4>Exponents</h4>
+        <p>
+          The exponents package teaches students about positive and negative
+          exponents, solving equations with exponents, and using scientific
+          notation.
+        </p>
+      </body>
+    </div>
+  );
 }
 
-export default Math
+export default Math;
