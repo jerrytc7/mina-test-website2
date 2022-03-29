6200584dfca65f39b2d4853280a17f55e6c6a979
jerrytc7 <jerrykondner@gmail.com>
2022-03-29 17:55:28 -0400

    added school policies page

--- src/components/information/Policies.js
+++ src/components/information/Policies.js
@@ -1,9 +1,111 @@
-import React from 'react'
+import React from "react";
 
 function Policies() {
   return (
-    <div>Policies</div>
-  )
+    <div>
+      <header>
+        <h3>School Policies</h3>
+      </header>
+      <body>
+        <h4>Non-Discrimination</h4>
+        <p>
+          Mina's School of Great Falls admits qualified students of any race,
+          religion, national or ethnic origin, disability sex, or sexual
+          orientation to all of the rights, privileges, programs, and activities
+          generally accorded or made available to our private school students.
+          We do not discriminate on the basis of race, religion, or national and
+          ethnic origin in the administration of its educational policies,
+          admissions policies, financial aid programs, or athletic and other
+          school programs.
+        </p>
+        <h4>Allergies & Medications</h4>
+        <p>
+          Parents must notify the school of known existing allergies. Mina's
+          School of Great Falls will also ask students to be sensitive to the
+          allergies of other students to avoid exposure. To avoid allergic
+          reactions in school, and deal with any that occur, other students and
+          parents are notified that children have particular allergies and told
+          which allergens to avoid sending to school. As required in Virginia,
+          private schools may only administer medications by certified staff
+          (those with MAT certification). Mina's Elementary School has staff
+          that are MAT certified. Parents must provide the school with detailed
+          parental consent forms, as supplied by the school (and Virginia's
+          Dept. of Social Services), and instructions from the physician when
+          required, and the medication to be administered.
+        </p>
+        <h4>Uniforms</h4>
+        <p>
+          Students should wear Navy Blue tops (shirt/blouse, etc.) and Khaki
+          bottoms (pants/shorts/skirts, etc.). Clean, practical shoes of any
+          type can be worn. Keep in mind that students will be allowed
+          adult-supervised outdoor recess twice a day at the nearby soccer
+          field, basketball court, or Great Falls Green and should be dressed
+          appropriately.
+        </p>
+        <h3>Student Saftey</h3>
+        <h4>Physical Building</h4>
+        <p>
+          Upon entering the school parents will find all exits are alarmed and
+          equipped with modern video equipment. Students are not permitted to
+          open doors during the school day. Only staff are allowed to admit
+          visitors and students to the premises or release students after school
+          or tutoring sessions. Students practice fire drills regularly to
+          ensure they know what to do in an emergency. Students are monitored by
+          staff members on school premises and also while on field trips.
+        </p>
+        <h4>Security Procedures & Review</h4>
+        <p>
+          We conduct an annual review of security procedures and retrain
+          teachers and staff annually. At the beginning of each school year
+          parents/guardians are asked to sign a form designating what adults are
+          permitted to pick up their child/children. The student/s will be
+          released only to adults on the list. Background checks are performed
+          on all school staff.
+        </p>
+        <h4>Technology Policy</h4>
+        <p>
+          The school has a strict technology safety policy to ensure students
+          view only appropriate Internet material when conducting academic
+          research projects. Teachers monitor students who have permission to
+          use the Internet.
+        </p>
+        <h4>Opt-Out Forms</h4>
+        <p>
+          <ul>
+            <li>
+              Form 1. Any parent or guardian who objects to his or her child’s
+              participation in any school activities.
+            </li>
+            <li>
+              Form 2. Any parent or guardian who objects to the release of
+              student information and images for publications, including use of
+              the student’s voice, image or name in a video, film, website,
+              social media or broadcast events.
+            </li>
+            <li>
+              Form 3. Any parent or guardian who objects to release of directory
+              information about students and staff.
+            </li>
+            <li>
+              Form 4. Any parent or guardian who objects to release of
+              information for commercial purposes.
+            </li>
+          </ul>
+        </p>
+        <h4>Inclement Weather Policy</h4>
+        <p>
+          The school follows the same cancellation policy as the Fairfax County
+          Public School system. You can click on the following web site for
+          school delays, closing and early dismissal information.
+          www.fcps.edu/alerts Due to the speed of updating, the official
+          @FCPSnews Twitter account is generally the first and fastest mode of
+          communication. In the case of emergency for a weather-related closing,
+          delay, or early dismissal, the FCPS website will automatically include
+          an alert at the top of each page with the appropriate information.
+        </p>
+      </body>
+    </div>
+  );
 }
 
-export default Policies
+export default Policies;
