beb03694bb0ff8455730e1b1b4c112f2087c248c
jerrytc7 <jerrykondner@gmail.com>
2022-03-29 18:07:59 -0400

    added philosophies information

--- src/components/about-us/Philosophies.js
+++ src/components/about-us/Philosophies.js
@@ -1,9 +1,107 @@
-import React from 'react'
+import React from "react";
 
 function Philosophies() {
   return (
-    <div>Philosophies</div>
-  )
+    <div>
+      <header>
+        <h3>Philosophies</h3>
+      </header>
+      <body>
+        <p>
+          Our experience suggests that exposing children to a small group
+          environment in school improves the student’s self-esteem, social
+          development, and confidence. There are many positive benefits that
+          come from a setting that helps children form deep friendships while
+          also requiring the child to learn problem-solving skills.
+        </p>
+        <h4>Healthy Relationships Among Students</h4>
+        <p>
+          In a small school bullying and other negative behaviors are dealt with
+          promptly and constructively. Children develop conflict resolution
+          skills which help them form healthy personal relationships throughout
+          their lives.
+        </p>
+        <h4>Real-World Problem-Solving Skills</h4>
+        <p>
+          Small-setting classes more closely resemble a real-world environment
+          in which small teams of individuals work closely together and,
+          therefore, must resolve issues with their peers. Many social skills,
+          such as small group collaboration, are acquired and practiced through
+          interactions in the school. Also, a small-group setting resembles a
+          family environment.
+        </p>
+        <h4>Better Self-Esteem</h4>
+        <p>
+          Unlike a small school environment, large settings expose children to
+          social cliques and heavy peer pressure as the child often is
+          encouraged by other students to stand out individually and test
+          authority to gain acceptance.
+        </p>
+        <h4>Emphasis on Basics</h4>
+        <p>
+          Emphasis is on basics which increase the attention of the children. We
+          practice this method consistently and successfully in each class.
+        </p>
+      </body>
+      <h3>Teaching Methodologies</h3>
+      <p>
+        We place our students in an appropriate level of educational instruction
+        for their skill level in each subject, rather than simply an aged-based
+        grade level. We conduct a detailed evaluation of every student to ensure
+        the child is taught the material he/she can handle while also challenged
+        to move up to the next level of difficulty.
+      </p>
+      <h4>Study Skills</h4>
+      <p>
+        After learning basic reading skills students learn how to study material
+        effectively. Ms. Mina’s method enables our students to study
+        independently. Our students acquire a strong work ethic and a solid
+        working knowledge of the subject matter.
+      </p>
+      <h4>No Skipped Steps</h4>
+      <p>
+        Each child must master the topic of the lesson before moving ahead to
+        ensure a high level of performance in all subjects. This is a key to our
+        success.
+      </p>
+      <h4>Smooth Progression</h4>
+      <p>
+        Many of our students at the 6th grade level are working on high school
+        geometry, without pressure from parents or the school. We also have
+        accepted children who are behind their grade level. By developing an
+        individualized curriculum for their studies we help them quickly and
+        smoothly advance in their program.
+      </p>
+      <h4>Proven Results</h4>
+      <p>
+        Providing a solid foundation for Algebra, Geometry, Trigonometry,
+        grammar, social studies, and science helps your child achieve
+        astonishing results. Students remain enthusiastically engaged in their
+        work, develop good self-esteem and confidence and are prepared to take
+        on high school classes with a positive attitude and intellectual
+        curiosity.
+      </p>
+      <h4>Parent Involvement</h4>
+      <p>
+        We encourage parents to support their children’s reading for pleasure at
+        home. The school also works closely with each parent to ensure the
+        child’s success.
+      </p>
+      <h3>ADHD</h3>
+      <p>
+        We agree with Dr. Jerome Kagan, a Harvard University professor, who
+        rejects the notion that a large percentage of children suffer from ADHD.
+        Many years of experience in teaching indicates the real problem is that
+        the majority of our children suffer from Teaching Deficit Disorder
+        (TDD). In our school, we have been able to take every child who has
+        walked through our doors and successfully teach them math, grammar,
+        writing, social studies, and science.​ Our success in teaching every
+        child in our school proves that the solution to learning is heavily
+        dependent on the teaching method and skills of the teacher, rather than
+        solely on the learning ability of the student.
+      </p>
+    </div>
+  );
 }
 
-export default Philosophies
+export default Philosophies;
