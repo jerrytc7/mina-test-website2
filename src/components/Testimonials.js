fd4c58f02c38883cfa083411c6f55c1f13337bb5
jerrytc7 <jerrykondner@gmail.com>
2022-04-04 22:57:15 -0400

    removed testimonial pic height

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -8,19 +8,18 @@ function Testimonials() {
       <h3>Testimonials</h3>
       <Carousel variant="dark">
         {reviews.map((d) => (
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="greybackground.avif"
-                alt="First slide"
-                height="500vh"
-              />
-              <Carousel.Caption>
-                <h5>{d.parentName}</h5>
-                <h6>{d.relationship}</h6>
-                <p>{d.review}</p>
-              </Carousel.Caption>
-            </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="greybackground.avif"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h5>{d.parentName}</h5>
+              <h6>{d.relationship}</h6>
+              <p>{d.review}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
         ))}
       </Carousel>
     </div>
