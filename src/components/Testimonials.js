5af570c65db5342c21147f4c03bd23e6d177a4f3
jerrytc7 <jerrykondner@gmail.com>
2022-04-04 20:00:55 -0400

    moved testimonial data into json file

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,4 +1,5 @@
 import React from "react";
+import reviews from "../data/testimonials.json";
 import { Carousel } from "react-bootstrap";
 
 function Testimonials() {
@@ -6,135 +7,21 @@ function Testimonials() {
     <div className="testimonial-container">
       <h3>Testimonials</h3>
       <Carousel variant="dark">
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="greybackground.avif"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h5>Chuck W.</h5>
-            <h6>Parent</h6>
-            <p>
-              Since enrolling our son in Mina's Sci-Math school, we have been
-              witnessing a miracle unfold before our eyes. Like most parents, we
-              wished for our son to be inspired and to find value in his school
-              work. Large classrooms and the "one size fits all" method of
-              public education, was simply not working. As a result, we decided
-              to seek an alternative. Immediately, it seemed everyone we queried
-              offered RAVE reviews about Miss Mina. Shortly after, we decided to
-              enroll our child. Miss Mina’s teaching style along with her warm
-              and nurturing spirit resulted in a transformation of our son's
-              attitude toward all of his subjects including: reading, writing,
-              math, science, history and more. His self-confidence continuous to
-              escalate and he is motivated to complete his assignments from a
-              position of inspiration. Miss Mina and her staff are terrific! Our
-              only wish is that we had started with Miss Mina sooner!!
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="greybackground.avif"
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h5>Paul B.</h5>
-            <h6>Parent</h6>
-            <p>
-              This school is truly amazing! My son has attended for the last
-              couple of years and has progressed in all respects, but especially
-              in science and math. He now performs at a level in math more than
-              two grade levels above where he would in public school. I have
-              always believed in the philosophy of investing early and often; I
-              am convinced that sending our son to Mina's school has been an
-              investment that will benefit my son for his entire life. If you
-              live near Great Falls and want to make a special investment in
-              your child, you will be hard pressed to find anything better.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="greybackground.avif"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h5>Nahid M.</h5>
-            <h6>Parent</h6>
-            <p>
-              My son attended Mina's Sci-Math Private School in Great Falls for
-              two years. Prior to attending this school his organizational
-              skills, grammar, mathematical concepts, reading comprehension, and
-              science were all very weak. He has since made dramatic
-              improvements in all of these areas. He can now organize his
-              thoughts easily and process information in mathematics and other
-              core subjects. Enrolling him in this private school has been the
-              best investment that I made for his life, and I am so happy that
-              my son is now prepared for future academic challenges.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="greybackground.avif"
-            alt="Fourth slide"
-          />
-          <Carousel.Caption>
-            <h5>Naeeme M</h5>
-            <h6>Parent</h6>
-            <p>
-              "The student to teacher ratio at Mina's Sci-Math Elementary,
-              coupled with the teaching methods that are incorporated with the
-              curriculum, are two of the many reasons why my nephew has started
-              to love school and studying. Because he feels more confident and
-              responsible now, he is beginning to make tremendous strides,
-              especially in Mathematics & Organized writing. Mina's Sci-Math
-              Elementary has been extremely effective and I vehemently recommend
-              it to those who live in Great Falls, McLean, Vienna, Reston, and
-              Herndon."
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="greybackground.avif"
-            alt="Fifth slide"
-          />
-          <Carousel.Caption>
-            <h5>Dr. Mary B.</h5>
-            <h6>Parent</h6>
-            <p>
-              "Our kindergartener has learned so much this year! The teachers
-              give a personalized program for each child. Imagine an environment
-              that helps a child learn in a caring, holistic environment. Our
-              child loves going to school ! Since the fall, she has learned
-              addition, reading, writing and she loves doing her "work" at home
-              without being asked. Amazing system and teaching methods!"
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="greybackground.avif"
-            alt="Sixth slide"
-          />
-          <Carousel.Caption>
-            <h5>Lisa K.</h5>
-            <h6>Former Student</h6>
-            <p>
-              "Hi Miss Mina! I just wanted to say thank you for being such an
-              amazing teacher and helping me learn to be smart. You have helped
-              me become who i am today and you have made me intelligent. so i
-              thank you for being such an amazing teacher!😊"
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+        {reviews.map((d) => (
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="greybackground.avif"
+                alt="First slide"
+                height="500vh"
+              />
+              <Carousel.Caption>
+                <h5>{d.parentName}</h5>
+                <h6>{d.relationship}</h6>
+                <p>{d.review}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+        ))}
       </Carousel>
     </div>
   );
