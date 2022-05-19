785020cc7a5e0856086390deb75fa4cba2c8f42d
jerrytc7 <jerrykondner@gmail.com>
2022-05-19 11:33:31 -0400

    adding contact form info

--- client/src/components/contact/ContactForm.js
+++ client/src/components/contact/ContactForm.js
@@ -1,27 +1,22 @@
-import React, {useRef} from "react";
-import emailjs from '@emailjs/browser';
-import { Button, Form } from "react-bootstrap";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { Button, Form, Container, Row, Col } from "react-bootstrap";
 
-function ContactForm({addToast}) {
+function ContactForm({ addToast }) {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs
-      .sendForm(
-        "gmail",
-        "template_v41hm7l",
-        form.current,
-        "XRHlsNIyRts0YTcX6"
-      )
+      .sendForm("gmail", "template_v41hm7l", form.current, "XRHlsNIyRts0YTcX6")
       .then(
         (result) => {
           addToast({
             title: "Thanks",
-            message: "Your email has been sent. We will respond shortly."
-          })
-          form.current.reset()
+            message: "Your email has been sent. We will respond shortly.",
+          });
+          form.current.reset();
           console.log(result.text);
         },
         (error) => {
@@ -31,29 +26,45 @@ function ContactForm({addToast}) {
   };
 
   return (
-    <Form onSubmit={sendEmail} ref={form}>
-      <Form.Group controlId="firstName">
-        <Form.Label>First Name</Form.Label>
-        <Form.Control name="firstName" required/>
-      </Form.Group>
-      <Form.Group controlId="lastName">
-        <Form.Label>Last Name</Form.Label>
-        <Form.Control name="lastName" required/>
-      </Form.Group>
-      <Form.Group controlId="email">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" name="email" required/>
-      </Form.Group>
-      <Form.Group controlId="phoneNumber">
-        <Form.Label>Phone Number</Form.Label>
-        <Form.Control type="tel" name="phoneNumber" required/>
-      </Form.Group>
-      <Form.Group controlId="message">
-        <Form.Label>Message</Form.Label>
-        <Form.Control as="textarea" name="message" required/>
-      </Form.Group>
-      <Button type="submit">Submit</Button>
-    </Form>
+    <Container>
+      <Row>
+        <Col>
+          <h1>Contact Us!</h1>
+        </Col>
+      </Row>
+      <Row>
+      <Col>
+          <p>
+            If you are interested in enrolling in our school, getting more
+            information regarding our after-school programs, or have some questions about out summer camps,
+            please submit a contact request and we will respond shortly!
+          </p>
+        </Col>
+      </Row>
+      <Form onSubmit={sendEmail} ref={form}>
+        <Form.Group controlId="firstName">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control name="firstName" required />
+        </Form.Group>
+        <Form.Group controlId="lastName">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control name="lastName" required />
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" name="email" required />
+        </Form.Group>
+        <Form.Group controlId="phoneNumber">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control type="tel" name="phoneNumber" required />
+        </Form.Group>
+        <Form.Group controlId="message">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" name="message" required />
+        </Form.Group>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Container>
   );
 }
 
