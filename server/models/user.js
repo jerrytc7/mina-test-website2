7179d3492cc75a757064a9ee9ce5aea9ae013fc5
jerrytc7 <jerrykondner@gmail.com>
2022-05-25 18:12:17 -0400

    trying to add post request to server

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,14 @@ const mongoose = require("mongoose")
 const {model, Schema} = mongoose
 
 const schema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
     email: {
         type: String,
         required: true
--- server/routes/api/user/index.js
+++ server/routes/api/user/index.js
@@ -1,9 +1,27 @@
 const express = require("express")
+const signUpUser = require("../../../models/user")
 
 const router = express.Router()
 
 router.get("/", (req, res)=> {
     res.send("Hi")
 })
+
+router.post("/", (req, res)=> {
+    const signedUpUser = new signUpUser({
+        firstName:req.body.firstName,
+        lastName:req.body.lastName,
+        email:req.body.email,
+        password:req.body.password,
+    })
+    signedUpUser.save()
+    .then(data => {
+        res.json(data)
+    })
+    .catch(error => {
+        res.json(error)
+    })
+})
+
 
 module.exports = router
