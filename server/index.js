f8b9921d43141f82cac1b532e94cfa44f63fae46
jerrytc7 <jerrykondner@gmail.com>
2022-06-02 16:15:17 -0400

    adding env connecting mongoDB

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const routes = require("./routes")
+const dotenv = require("dotenv")
+const cors = require("cors")
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(routes)
+app.use(cors())
 
-app.listen(3001)
+dotenv.config()
+
+mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("Database Connected"))
+
+app.listen(3001, () => console.log("server is up and running"))
